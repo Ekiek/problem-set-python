e238c9de53d6ab386c91c9ce716554df687cfd9f
Ekiek <kakablishvili@yahoo.com>
2025-11-01 22:22:20 +0000

    Upload new problems

--- pizza/pizza.py
+++ pizza/pizza.py
@@ -1,3 +1,26 @@
 import sys
 import os
+import csv
+from tabulate import tabulate
 
+def main():
+    if len(sys.argv) != 2:
+        sys.exit("Too many command-line arguments")
+
+    filename = sys.argv[1]
+
+    if not filename.endswith(".csv"):
+        sys.exit("Not a CSV file")
+
+    if not os.path.isfile(filename):
+        sys.exit("File does not exist")
+    try:
+        with open(filename, newline="") as file:
+            reader = csv.reader(file)
+            table = list(reader)       
+    except FileNotFoundError:
+        sys.exit("File not found")
+    print(tabulate(table[1:], headers=table[0], tablefmt="grid"))
+    
+if __name__ == "__main__":
+    main()
